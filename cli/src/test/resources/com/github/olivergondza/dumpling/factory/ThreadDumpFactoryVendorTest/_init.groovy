4ecc367108e947feb86dc9fb917f20e6719ae53b
Oliver Gondža <ogondza@gmail.com>
2016-02-09 11:40:15 +0100

    forgotten debug statement

--- cli/src/test/resources/com/github/olivergondza/dumpling/factory/ThreadDumpFactoryVendorTest/_init.groovy
+++ cli/src/test/resources/com/github/olivergondza/dumpling/factory/ThreadDumpFactoryVendorTest/_init.groovy
@@ -1,4 +1,3 @@
-println D.args
 def pid = java.lang.management.ManagementFactory.getRuntimeMXBean().getName().replaceAll(/@.*/, '');
 def file = new File(D.args[0]);
 if (file.exists()) file.delete();
